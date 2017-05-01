c932ad8b72d65b1e91faccee6502c72e8370980d
Nathan Smith <nathan.smith5@mail.mcgill.ca>
2017-05-01 16:12:30 -0400

    Add 0.5 Compatability

--- src/PyGen.jl
+++ src/PyGen.jl
@@ -3,12 +3,70 @@ module PyGen
 using MacroTools
 
 export @pygen
+
+if VERSION > v"0.6-"
+
+macro pygen(f)
+
+    # generate a new symbol for the channel name and 
+    # wrapper function
+    c = gensym()
+    η = gensym()
+
+    # yield(λ) → put!(c, λ)
+    f′ = MacroTools.postwalk(f) do x
+        @capture(x, yield(λ_)) || return x
+        return :(put!($c, $λ))
+    end 
+    
+    # Fetch the function name and args
+    @capture(f′, function func_(args__) body_ end)
+
+    # wrap up the η function
+    final = quote 
+        function $func($(args...))
+            function $η($c)
+                $body
+            end
+            return Channel(c -> $η(c))
+        end
+    end
+
+    return esc(final)
+end
+
+else  # for versions less than v0.6-
+
+macro pygen(f)
+    η = gensym()
+    
+    f′ = MacroTools.postwalk(f) do x
+        @capture(x, yield(λ_)) || return x
+        return :(produce($λ))
+    end
+
+    @capture(f′, function func_(args__) body_ end)
+
+    final = quote
+        function $func($(args...))
+            function $η()
+                $body
+            end
+            return Task($η)
+        end
+    end
+    
+    return esc(final)
+end
+
+end
+
 """
     @pygen
 
-Making julie walking a little *more* like python. Stick 
+Making julia walk a little *more* like python. Stick 
 the @pygen macro in front of a function declaration and
-you can use `yield` statements to make a python style
+you can use `yield(foo)` statements to make a python style
 generator!
 
 ## Examples
@@ -39,33 +97,6 @@ julia> for n in fibonacci()
 .
 ``` 
 """
-macro pygen(f)
+:(@pygen)
 
-    # generate a new symbol for the channel name and 
-    # wrapper function
-    c = gensym()
-    η = gensym()
-
-    # yield(λ) → put!(c, λ)
-    f′ = MacroTools.postwalk(f) do x
-        @capture(x, yield(λ_)) || return x
-        return :(put!($c, $λ))
-    end 
-    
-    # Fetch the function name and args
-    @capture(f′, function func_(args__) body_ end)
-
-    # wrap up the η function
-    final = quote 
-        function $func($(args...))
-            function $η($c)
-                $body
-            end
-            return Channel(c -> $η(c))
-        end
-    end
-
-    return esc(final)
 end
-
-end 
