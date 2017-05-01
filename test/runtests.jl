a348fcc127566f2d98fffdc24dc07a30234f3ce1
Nathan Smith <nathan.smith5@mail.mcgill.ca>
2017-05-01 10:34:14 -0400

    more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,3 +13,25 @@ end
 
 # Test for \n in first line issue
 @test isdefined(:λ) == true
+
+# Test that the poodle function doesn't error out
+@pygen "
+function 🐩()
+    yield \"🐩\"
+end
+"
+
+@test isdefined(:🐩) == true
+
+# Test that one of these generators actually works
+@pygen """
+function squares(n)
+    i = 0
+    while i <= n
+        yield i^2
+        i += 1
+    end
+end
+"""
+
+@test sum(squares(10)) == sum(i^2 for i in 1:10)
