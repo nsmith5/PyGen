fa2f14364e842c3961cf0241575e67be2e0887e8
Nathan Smith <nathan.smith5@mail.mcgill.ca>
2017-04-30 23:18:20 -0400

    initial commit of code

--- src/PyGen.jl
+++ src/PyGen.jl
@@ -1,5 +1,55 @@
 module PyGen
 
-# package code goes here
+export pygen
+#=
+    @pygen is a macro that allows for python like generator functions.
+The idea is that a function like this:
 
-end # module
+function f(args)
+    ...
+    yield x
+    ...
+    yield y
+    ...
+end
+
+should become....
+
+function f(args)
+    function γ(c::Channel)
+        ...
+        put!(c, x)
+        ...
+        put!(c, y)
+        ...
+    end
+    return Channel(γ)
+end
+=#
+
+const yregex = r"(yield .*)"
+
+
+function asput(str)
+    # Convert "yield λ" to "put!(c, λ)
+    λ = replace(str, "yield ", "")
+    return "put!(c, $λ)"
+end
+
+
+"""
+    @pygen
+
+Making julie walking a little *more* like python. Stick the @pygen macro in front of a 
+function declaration and you can use `yield` statements to make a python style
+generator! 
+"""
+macro pygen(f)
+    f′ = replace(f, yregex, asput)
+    lines = f′ |> IOBuffer |> readlines
+    insert!(lines, 2, "function γ(c::Channel)")
+    push!(lines, "return Channel(γ)\nend")
+    return join(lines, "\n") |> parse |> esc 
+end
+
+end 
